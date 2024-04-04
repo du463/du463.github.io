1743d837aa9dd1084eb4a28783b761aee088384c
du463 <3111257719@qq.com>
2024-04-04 19:26:40 +0800

    Site updated: 2024-04-04 19:26:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/03/Codeforces Ronud 892（Div.2）/","2024/04/03/hello-world/"];function toRandomPost(){
+var posts=["2024/04/03/Codeforces Ronud 892（Div.2）/","2024/04/04/Codeforces Round 931 (Div. 2)/","2024/04/03/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
