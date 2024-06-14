76be884d36d7c553e8dc45e49cdddffe0adc13fa
du463 <3111257719@qq.com>
2024-06-14 08:25:53 +0800

    Site updated: 2024-06-14 08:25:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/03/Codeforces Ronud 892（Div.2）/","2024/04/04/Codeforces Round 931 (Div. 2)/","2024/06/13/牛客第46场周赛/","2024/04/03/hello-world/"];function toRandomPost(){
+var posts=["2024/04/03/Codeforces Ronud 892（Div.2）/","2024/04/03/hello-world/","2024/06/13/牛客第46场周赛/","2024/04/04/Codeforces Round 931 (Div. 2)/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
